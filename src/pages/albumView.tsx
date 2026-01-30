13b73c5b3b755004015dfbd92e5e7eed9088ff01
Africk-Hunter <hunterafrick@gmail.com>
2026-01-29 18:09:41 -0800

    Album View Page Styles
    - Added styles (currently only large desktop) to Album View page.

--- src/pages/albumView.tsx
+++ src/pages/albumView.tsx
@@ -23,23 +23,24 @@ const AlbumView: React.FC = () => {
     }
 
     return (
-        < main className="albumView" >
-            <button className="back" onClick={() => navigate('/')}><img src="../../public/images/Arrow.svg" alt="Return" className="backImg" /></button>
+        <main className="albumView" >
+            <button className="back" onClick={() => navigate('/')}><img src="../../public/images/Arrow.svg" alt="Return" /></button>
             <section className="albumInfo">
                 <img src={album.image_url} alt="" className="cover" />
                 <section className="middleInfo">
                     <div className="nameAndArtist">
-                        <h1 className="albumTitle">{album.name}</h1>
-                        <h2 className="albumArtist">{album.artist}</h2>
+                        <h1 className="albumTitleView">{album.name}</h1>
+                        <h2 className="albumArtistView">{album.artist}</h2>
                     </div>
                     <section className="links">
-                        <a href={album.spotify} className="link"><img src="" alt="Listen on Spotify" className="linkImg" /></a>
-                        <a href={album.apple} className="link"><img src="" alt="Listen on Apple Music" className="linkImg" /></a>
-                        <a href={album.bandcamp} className="link"><img src="" alt="Listen on Bandcamp" className="linkImg" /></a>
-                        <a href={album.amazon} className="link"><img src="" alt="Listen on Amazon Music" className="linkImg" /></a>
+                        <a href={album.spotify} target='_blank' className="link"><img src="../../public/images/spotify.png" alt="Listen on Spotify" className="linkImg" /></a>
+                        <a href={album.apple} target='_blank' className="link"><img src="../../public/images/AppleMusic.png" alt="Listen on Apple Music" className="linkImg" /></a>
+                        <a href={album.bandcamp} target='_blank' className="link"><img src="../../public/images/bandcamp.png" alt="Listen on Bandcamp" className="linkImg" /></a>
+                        <a href={album.amazon} target='_blank' className="link"><img src="../../public/images/amazon.svg" alt="Listen on Amazon Music" className="linkImg" /></a>
                     </section>
-                    <p className="genres"></p>
+                    
                 </section>
+                <p className="genres">Genres: {album.genres}</p>
             </section>
             <section className="albumReviews">
                 <div className="artistPitch">
